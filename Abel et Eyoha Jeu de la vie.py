de7580baac681f74ef3623a9535ac8b5ca65de98
AbelTewodros <73427595+AbelTewodros@users.noreply.github.com>
2024-10-15 16:36:51 -0700

    Delete Abel et Eyoha Jeu de la vie.py

--- Abel et Eyoha Jeu de la vie.py
+++
@@ -1,9 +0,0 @@
-def roman_int(s,l={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}):
-    if len(s)==1:
-        return l[s[0]]
-    if l[s[0]]<l[s[1]]:
-        return roman_int(s[1:])-l[s[0]]
-    return roman_int(s[1:])+l[s[0]]
-    
-
-print(roman_int("III"))
